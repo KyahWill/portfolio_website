c0dbf84d6a7ffb5bcc2d0a15a00a1b9701a5d180
KyahWill <knownbutnotloved@gmail.com>
2023-02-13 19:32:03 +0800

    added some details with the about me page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,24 +10,42 @@
     <div class="hero-content text-center">
         <div class="max-w-md">
         <h1 class="text-5xl font-bold">Will Vincent Parrone</h1>
-        <p class="py-6">Business Analyst, Rotarian, Something something portfolio</p>
-        <button class="btn btn-primary">Contact Me</button>
+        <p class="py-6">Business Analyst, Rotarian, Generalist Extraordinaire</p>
         </div>
     </div>
 </section>
 
 <section class="mb-10 mt-10">
+    <h1 class="text-3xl text-center ">
+        About me
+    </h1>
+    <section class="w-96 md:w-1/3 text-justify mx-auto mb-5 md:mb-0">
+        <p> I actually am not sure what to add in here but I generally have a lot of things that I know off and a lot of things I don't know about..</p>
+        <a href="/about">
+            <button class="text-secondary ">View more</button>
+        </a>
+    </section>
+</section>
+<section class="mb-10 mt-10">
     <h1 class="text-3xl text-center">
         Projects
     </h1>
 </section>
-<section class="flex flex-col-reverse md:flex-row items-center md:items-start ">
-    <section class="flex flex-col md:flex-row w-2/3 flex-wrap mx-5 ">
+<section class="flex flex-col-reverse md:flex-row items-center md:items-start mx-auto">
+    
+    <section class="flex flex-col flex-wrap md:flex-row md:w-7/12  mx-1 ">
         {#each projects as project}
         <ProjectCard project={project}/>
         {/each}
     </section>
-    <section class="w-2/3 md:w-1/3 text-justify mb-5 md:mb-0 mx-5">
+    
+    <section class=" text-justify mb-5 mx-auto w-96 md:w-5/12 md:mb-0 ">
         <p>I have built an expertise using various languages such as javascript, python, rust, and the likes. Attached within are the original projects that I have worked on or are still currently being worked on.</p>
+        <a href="/projects">
+            <button class="text-secondary ">View All Projects</button>        </a>
     </section>
+
+</section>
+<section>
+
 </section>
