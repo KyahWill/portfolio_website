baffe6a35d3f7723b0ed125ee9c7575e1c8ab6a9
KyahWill <knownbutnotloved@gmail.com>
2023-03-03 20:29:22 +0800

    added additional tests for this group

--- src/routes/(test)/old/+layout.svelte
+++ src/routes/(test)/old/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import NavMenu from "$lib/NavMenu.svelte";
-  import "../app.css";
+  import "../../../app.css";
   import Transition from "$lib/Transition.svelte";
   import { page } from "$app/stores";
   import Footer from "$lib/Footer.svelte";
